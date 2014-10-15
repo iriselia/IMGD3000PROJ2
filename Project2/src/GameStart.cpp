6769e8b94eb09ad2c1210a2f9be6633b31474444
jordanlk <jordan.silvaa42@gmail.com>
2014-10-14 22:48:18 -0400

    pushing

--- Project2/src/GameStart.cpp
+++ Project2/src/GameStart.cpp
@@ -112,17 +112,19 @@ void GameStart::start()
 	new Platform(false, 50, 6, 780, 55, 0.0, 0.3); // size, height, x, y, xspeed, yspeed
 	new Platform(true, 126, 6, 835, 70, 0.0, 0.0); // size, height, x, y, xspeed, yspeed
 	new Platform(true, 250, 20, 920, 50, 0.0, 0.0); // size, height, x, y, xspeed, yspeed
-
+	new MadSquare(1000, 40);
 	//Area 3
 	new Platform(false, 110, 10, 1180, 55, 0.3, 0.0); // down
+	new Suicider(1290, 52);
 	new Platform(true, 75, 5, 1173, 45, 0.0, 0.15); // up
 	new Platform(true, 30, 4, 1222, 33, 0.0, 0.0); // up
+	new MadSquare(1230, 29);
 	new Platform(true, 30, 4, 1260, 33, 0.0, 0.0); // up
 	new Platform(true, 30, 4, 1299, 33, 0.0, 0.0); // up
 	new Platform(false, 110, 30, 1295, 50, 0.0, 0.0); // down
 	new Platform(false, 90, 5, 1409, 45, 0.0, 0.0); // down
 	new Platform(false, 20, 5, 1509, 39, 0.4, 0.0); // down
-	new Platform(false, 50, 7, 1531, 31, 0.0, 0.0); // down
+	new Platform(false, 50, 7, 1531, 33, 0.0, 0.0); // down
 
 	//new Platform(false, 35, 10, 550, 45, 0.2, 0.0); // size, height, x, y, xspeed, yspeed
 	//new Platform(true, 200, 5, 590, 48, 0.0, 0.0); // enemies here
@@ -135,8 +137,8 @@ void GameStart::start()
 	//new MadSquare(45, 7);
 
 
-	//new WinFlag(40, 20);
-	new Hero(1450, 10);
+	new WinFlag(1545, 31);
+	new Hero(1150, 10);
 	//new Hero(380, 10);
 
 	//   ViewObject *p_vo = new ViewObject; // Count of nukes.
